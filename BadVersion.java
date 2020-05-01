e805e94d06ae618309aff4ef9cbfc97b26b66d25
ES\harsulkt <tulika.sunil.harsulkar@adp.com>
2020-05-01 14:21:55 -0400

    Added leetcode accepted solution

--- BadVersion.java
+++ BadVersion.java
@@ -1,24 +1,44 @@
 package mayChallenge;
 
+// ***************************************LEETCODE SOLUTION***********************************************************
+//public class Solution extends VersionControl {
+//    public int firstBadVersion(int n) {
+//        int start = 1;
+//		int end = n;
+//
+//
+//           while(start < end) {
+//			int mid = start + (end-start)/2;
+//			if(isBadVersion(mid) == false) { 
+//				start = mid+1;				
+//			} else {
+//			    end = mid;
+//			}
+//		} 
+//        	
+//		return start;
+//}
+//}
+//
+
+
 public class BadVersion {
 	
 	
 
 	public static void main(String[] args) {
-		String n[] = {"false", "true", "true", "true", "true"};
+		String n[] = {"false","false","false","true","true"};
 		int result = getVersion(n);
 		System.out.println(result);
 	}
 	
 	private static int getVersion(String[] n) {
-		int start = 0;
-		int end = n.length-1;
-		
-		//0 - false;
-		//1 - true;
+		int start = 1;
+		int end = n.length;
+
 		while(start < end) {
-			int mid = (start + (end - start))/2;
-			if(n[mid] == "false") { //call the api to get the boolean value
+			int mid = start + end/2;
+			if(n[mid] == "false") { 
 				start = mid+1;				
 			} else {
 			end = mid;
